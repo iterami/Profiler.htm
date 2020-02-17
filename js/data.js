faf5b8be569bf221ff04b6b68e1dd7f6a4286d92
Jan Hořava <czechinthemail@gmail.com>
2020-02-17 00:00:10 -0800

    replaced some let with const

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@
 function profile(){
     core_storage_save();
 
-    let result = test_time({
+    const result = test_time({
       'function': eval(core_storage_data['function']),
       'function-args': JSON.parse(core_storage_data['args']),
       'runs': core_storage_data['runs'],
