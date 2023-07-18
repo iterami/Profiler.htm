14d20c94d86b834049816feb060e0cdde64caa01
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:20:53 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function profile(){
+    core_storage_save();
+
+    const result = test_time({
+      'function': eval(core_storage_data['function']),
+      'function-args': JSON.parse(core_storage_data['args']),
+      'runs': core_storage_data['runs'],
+    });
+
+    core_ui_update({
+      'ids': {
+        'average': result['average'],
+        'max': result['max'],
+        'min': result['min'],
+        'total': result['total'],
+      },
+    });
+}
+
 function repo_init(){
     core_repo_init({
       'events': {
