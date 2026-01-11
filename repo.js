94f2fcb77418a129473a8912376c60003a0609c4
Jan Hořava <czechinthemail@gmail.com>
2026-01-11 01:19:40 -0800

    removed storage save from profile()

--- repo.js
+++ repo.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function profile(){
-    core_storage_save();
-
     const testing = globalThis.eval(core_storage_data.function);
     if(core_type(testing) !== 'function'){
         return;
